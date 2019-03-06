34c6d87d38b2b3a9207e0781dc3781cf172b6496
Amanda Steigerwalt <asteig@gmail.com>
2019-03-06 15:36:37 -0800

    Added burger control buttons

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,22 +4,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
-    let transformedIngredients = Object.keys( props.ingredients )
-        .map( igKey => {
-            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
-                return <BurgerIngredient key={igKey + i} type={igKey} />;
-            } );
-        } )
-        .reduce((arr, el) => {
-            return arr.concat(el)
-        }, []);
-    if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>;
+    
+    const ingredients = [];
+
+    Object.keys(props.ingredients).map((key) => {
+        for(var i = 0; i < props.ingredients[key]; i++) {
+            ingredients.push(<BurgerIngredient key={key+i} type={key} />);
+        }
+    });
+
+    if(!ingredients.length) {
+        ingredients = "Please add ingredients.";
     }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
+                {ingredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BurgerControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {...}
-    // }
+
     state = {
         ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
+            salad: 4,
+            bacon: 4,
+            cheese: 4,
+            meat: 4
         }
     }
 
@@ -21,7 +19,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BurgerControls />
             </Aux>
         );
     }
