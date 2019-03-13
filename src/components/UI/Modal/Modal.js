e60898d952d8b16e4d400ad2b62b1e17e17bf1d5
Amanda Steigerwalt <asteig@gmail.com>
2019-03-13 15:11:50 -0700

    Basic finished

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
 
-    shouldComponentUpdate(nextProps, nextState) {
+    shouldComponentUpdate ( nextProps, nextState ) {
         return nextProps.show !== this.props.show;
     }
 
-    componentWillUpdate() {
-        console.log('Modal [componentWillUpdate]');
+    componentWillUpdate () {
+        console.log('[Modal] WillUpdate');
     }
 
-    render() {
+    render () {
         return (
             <Aux>
                 <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
@@ -27,7 +27,7 @@ class Modal extends Component {
                     {this.props.children}
                 </div>
             </Aux>
-        );
+        )
     }
 }
 
